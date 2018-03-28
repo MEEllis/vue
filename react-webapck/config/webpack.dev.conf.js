3529ed05e544dc2389ccd1ce88a50d464307dcfc
MEEllis <1641919800@qq.com>
2018-03-28 19:23:55 +0800

    testa

---
+++ react-webapck/config/webpack.dev.conf.js
@@ -0,0 +1,28 @@
+//开发环境打包
+var webpack =require('webpack')
+var merge = require('webpack-merge')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var baseWebpackConfig = require('./webpack.base.conf')
+module.exports = merge(baseWebpackConfig, {
+  entry: {
+    app: [
+      'webpack-hot-middleware/client?reload=true',
+      'webpack/hot/only-dev-server',
+      './src/index.js'
+    ],
+    'babel-polyfill': 'babel-polyfill',
+    vendors: ['react', 'react-dom', 'react-router-dom']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendors'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: '../public/index.html',
+      inject: true
+    })
+  ]
+})
