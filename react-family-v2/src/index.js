58f7c0a29c00b8b99f398660a77b626bf12e2045
MEEllis <1641919800@qq.com>
2018-04-26 18:51:51 +0800

    sdfsdfds

--- react-family-v2/src/index.js
+++ react-family-v2/src/index.js
@@ -25,5 +25,5 @@ app.router(require('./router').default);
 
 // 5. Start
 app.start('#root');
-
+window.app = app;
 export default app._store; // eslint-disable-line
--- react-family-v2/src/models/login.js
+++ react-family-v2/src/models/login.js
@@ -12,7 +12,9 @@ export default {
 
   effects: {
     *login({ payload }, { call, put }) {
+      // 用于调用异步逻辑，支持 promise 。
       const response = yield call(fakeAccountLogin, payload);
+      // 用于触发 action
       yield put({
         type: 'changeLoginStatus',
         payload: response,
@@ -24,7 +26,7 @@ export default {
       }
     },
   },
-
+  // 以 key/value 格式定义 reducer。用于处理同步操作，唯一可以修改 state 的地方。由 action 触发。
   reducers: {
     changeLoginStatus(state, { payload }) {
       setAuthority(payload.currentAuthority);
@@ -35,4 +37,10 @@ export default {
       };
     },
   },
+  /**
+   * 以key/value 格式定义 subscription。subscription 是订阅，用于订阅一个数据源，
+   * 然后根据需要 dispatch 相应的 action。在 app.start() 时被执行，
+   * 数据源可以是当前的时间、服务器的 websocket 连接、keyboard 输入、geolocation 变化、history 路由变化等等。
+   */
+  subscriptions: {},
 };
