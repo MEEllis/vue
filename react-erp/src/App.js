45b69dfd52403b58a749cdcb9947ab9e4621abf0
MEEllis <1641919800@qq.com>
2018-11-19 18:05:56 +0800

    aaaaaaaa

---
+++ react-erp/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Row, Col, Button } from 'antd';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="login-container">
+        <canvas id="noise-canvas"></canvas>
+        <Row type="flex" justify="center" align="middle">
+          <Col>
+            aaaa
+          </Col>
+
+        </Row>
+        <Button>aaa</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
