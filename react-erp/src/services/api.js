287770a4ecc9c9d751153e2f4075f2d1287b3a8a
MEEllis <1641919800@qq.com>
2018-12-04 18:07:01 +0800

    dddddd

--- react-erp/src/services/api.js
+++ react-erp/src/services/api.js
@@ -1,13 +1,16 @@
 import request from '@/utils/request'
 import qs from 'qs'
-export function loginByUsername(q, password) {
-    const params = {
-      q:"javascript",
+export function loginByUsername(username, password) {
+    const data = {
+      username,
+      password,
+      authCode:'jacc',
+      cid:-1
     }
     return request({
-      url: '/v2/book/search',
-      method: 'get',
-      params,
+      url: '/manager/emp/empLoginAjax.do',
+      method: 'post',
+      data: qs.stringify(data),
       loading: 'spin'
     })
   }
